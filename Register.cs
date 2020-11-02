99dfa12f5db5b14a5180514b8cd2d3810a96f9ef
netivan <72153714+netivan@users.noreply.github.com>
2020-11-02 21:04:28 +0100

    Add files via upload

---
+++ Register.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using System.Threading;
+using System.Linq;
+
+namespace Hamnen
+{
+    class Register
+    {
+        const int xPosHmReg = 50, yPosHmReg = 7;
+
+       public static List<Båt> HamnRegister = new List<Båt>();   // registret av hamnen i vilken alla inkommande båtar läggs in. ( i början är tom)
+        public static void SkrivutHamnaregister(int day)  
+
+        {
+ /* var = IEnumerable*/   var dagensRegister = from b in HamnRegister   // dagenRegister är en lista , det finns alla båtar som är i hamnen en x dag.   
+                    where (day - b.aDag) < b.dagarIhamnen     // (pågående dag - ankomstdagen av båten) ska vara < dagarna båten ska stanna i hamnen
+                    orderby b.kajPlats
+                    select b;
+            
+           // flotta(day);      // räcknar båtarna enligt typ som finns i kajen en x dag   (ex. Motorbåtar[5])
+           // statistik(day);
+            
+            int ypos = yPosHmReg;
+            SetCursorPosition(xPosHmReg, ypos++);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Write("       H A M N R E G I S T R E T       ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            
+            string sPlats, övrigt;
+            foreach (var b in dagensRegister)       // skriver ut alla båtar som finns i listan q 
+            {
+                sPlats = b.kajPlats.ToString();
+                if (b.antalPlatser > 1)
+                    sPlats += "-" + (b.kajPlats + b.antalPlatser - 1).ToString();
+                //sPlats += "".Substring(0, b.antalPlatser - 1);
+
+                övrigt = b.övrigt.Beskrivning + b.övrigt.value.ToString() + b.övrigt.mått;
+
+                SetCursorPosition(xPosHmReg, ypos++);
+
+                Write("{0,-5} [{1,5}] ", sPlats, b.Identitetsnummer);
+                Console.ForegroundColor = b.färg;
+                Write(" {0,-10}  ", b.typ);
+                Console.ForegroundColor = ConsoleColor.White;
+                Write("{0,-15} ", övrigt);
+           
+            }
+            for (int i = 1; i < 7 && ypos < 40; i++)
+            {
+                SetCursorPosition(xPosHmReg, ypos++);
+                WriteLine($"                                                                   ");
+            }
+        }
+        public static void flotta(int day)
+        {
+            var q1 = from b in HamnRegister         // delat i grupp alla båttyper.   q1 är 4 tabeller dvs en tabel för varje båt i vilken finns alla egenskaper av varje båt.
+                     where (day - b.aDag ) < b.dagarIhamnen
+                     group b by b.typ;
+
+            SetCursorPosition(85, 4);
+
+            int totalBåtar = (from t in q1
+                             select t.Count()).Sum();         // från t (tabell) som finns i q1, (count) räcknar alla båtar som är i varje tabell.  Sum()  summerar
+
+            foreach (var z in q1)
+            {
+                Write("{0,2} [{1,2}]", z.Key, z.Count());
+            }
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            SetCursorPosition(85, 3);
+            Write($" Båtar som finns på kajen {totalBåtar}");
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        public static void statistik(int day)    
+        {
+            var totalVikt = (from b in HamnRegister
+                     where (day - b.aDag) < b.dagarIhamnen
+                     select b.vikt).Sum();
+
+
+            var medelHastighet = (from b in HamnRegister
+                                  where (day - b.aDag) < b.dagarIhamnen
+                                  select b.maxHastighet).Average();
+
+            double ledigaPlatser = Kaj.ledigaPlatser();
+
+            Console.SetCursorPosition(2, 36);
+           // Console.Write($"Lediga Platser: {ledigaPlatser}  Total Vikt: {totalVikt}  Medelhastighet: {medelHastighet}  ");
+            Console.Write("Lediga Platser: {0,2}  Total Vikt: {1,-7}  Medelhastighet: {2, 6:N1}  ", ledigaPlatser, totalVikt, medelHastighet);
+
+        }
+    }
+}
---
+++ Skärm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using System.Threading;
+using System.Linq;
+
+namespace Hamnen
+{
+    class Skärm : Mother
+    {
+        const int xPosIn = 3, yPosIn = 7;       // Skriver ut titeln  "Inkommande båtar"
+        const int xPosUt = 3, yPosUt = 20;            //  skriver ut titeln "Utgående båtar"
+                                                      //const int xPosKaj = 9, yPosKaj = 4;
+        public static int yPositionUt = yPosUt + 1;
+        public static int yPositionIn = yPosIn + 1;
+
+        public static void Titel()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            SetCursorPosition(40, 3);
+            Console.WriteLine("Stockholms Hamn");
+            SetCursorPosition(1, 3);
+            Write("KAJ -> |");
+            SetCursorPosition(9, 3);
+            Write(" 0.........10........20........30.........40........50........60...|");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(xPosIn, yPosIn);
+            Console.WriteLine("  Inkommande Båtar  ");
+
+            Console.SetCursorPosition(xPosUt, yPosUt);
+            Console.WriteLine("  Utgående Båtar  ");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public static void skrivaDay(int day)
+        {
+            Console.SetCursorPosition(11, 1);
+
+            Console.WriteLine($"Dag {day}");      // pågående dag
+
+            Console.SetCursorPosition(80, 1);
+            Console.WriteLine($"Dag {day}");
+
+        }
+        public static void skrivaIngående(string s)
+        {
+            SetCursorPosition(xPosIn, yPositionIn++);
+            Write(s);
+        }
+
+        public static void skrivautgående(string s)
+        {
+            Console.SetCursorPosition(xPosUt, yPositionUt++);
+            Console.Write(s);
+        }
+        public static void cancelday()
+        {
+
+            for (int i = yPosIn + 3; i < yPositionIn; i++)
+            {
+                SetCursorPosition(xPosIn, i);
+                Write("                                       ");
+            }
+
+            for (int i = yPosUt + 3; i < yPositionUt; i++)
+            {
+                SetCursorPosition(xPosUt, i);
+                Write("                                             ");
+            }
+
+            yPositionUt = yPosUt + 1;
+            yPositionIn = yPosIn + 1;
+        }
+          
+        public static bool pausa(int delay)
+        {
+            for (int i = 1; i < delay; i++)
+            {
+                Thread.Sleep(1);   // ny dag
+                if (Console.KeyAvailable == true)
+                    return true;
+            }
+            return false;
+
+        }
+    }
+}
